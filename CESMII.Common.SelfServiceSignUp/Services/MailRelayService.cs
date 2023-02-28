ac217ebaf66b18747803f2379082e123e8cf9a1b
Paul Yao <paul@paulyao.com>
2023-02-28 15:13:51 -0500

    Email - Testing / debugging

--- CESMII.Common.SelfServiceSignUp/Services/MailRelayService.cs
+++ CESMII.Common.SelfServiceSignUp/Services/MailRelayService.cs
@@ -16,6 +16,7 @@
     using CESMII.Common.SelfServiceSignUp.Models;
     using CESMII.Common;
     using Microsoft.AspNetCore.Components.Web;
+    using System.Text;
 
 #pragma warning disable 8601, 8602, 8604  // Suppress warnings that items might have a null value.
     public class MailRelayService
@@ -31,112 +32,6 @@
             _logger = logger;
         }
 
-        //public async Task<bool> SendEmail(MailMessage message)
-        //{
-        //    switch (_config.Provider)
-        //    {
-        //        case "SMTP":
-        //            if (!await SendSmtpEmail(message))
-        //            {
-        //                return false;
-        //            }
-
-        //            break;
-        //        case "SendGrid":
-        //            if (!await SendEmailSendGrid(message))
-        //            {
-        //                return false;
-        //            }
-        //            break;
-        //        default:
-        //            _logger.LogError("MailRelayService::SendEmail - Provider key value is invalid");
-        //            throw new InvalidOperationException("The configured email provider is invalid.");
-        //    }
-        //    return true;
-        //}
-
-        //private async Task<bool> SendSmtpEmail(MailMessage message)
-        //{
-        //    // Do not proceed further if mail relay is disabled.
-        //    if (!_config.Enabled)
-        //    {
-        //        _logger.LogWarning("Mail Relay is disabled.");
-        //        return true;
-        //    }
-
-        //    // Configure the SMTP client and send the message
-        //    var client = new SmtpClient
-        //    {
-        //        Host = _config.Address,
-        //        Port = _config.Port,
-
-        //        // Use whatever SSL mode is set.
-        //        EnableSsl = _config.EnableSsl,
-        //        DeliveryMethod = SmtpDeliveryMethod.Network
-        //    };
-
-        //    if (_config.EnableSsl)
-        //    {
-        //        ServicePointManager.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
-        //    }
-
-        //    _logger.LogDebug($"Email configuration | Server: {_config.Address} Port: {_config.Port} SSL: {_config.EnableSsl}");
-
-        //    message.From = new MailAddress(_config.MailFromAddress, "SM Marketplace");
-
-        //    // If Mail Relay is in debug mode set all addresses to the configuration file.
-        //    if (_config.Debug)
-        //    {
-        //        _logger.LogDebug($"Mail relay is in debug mode. Redirecting target email to: {string.Join("|", _config.DebugToAddresses)}");
-        //        message.To.Clear();
-        //        foreach (var address in _config.DebugToAddresses)
-        //        {
-        //            message.To.Add(address);
-        //        }
-        //    }
-
-        //    else
-        //    {
-        //        message.To.Clear();
-        //        foreach (var address in _config.ToAddresses)
-        //        {
-        //            message.To.Add(address);
-        //        }
-        //    }
-
-        //    // If the user has setup credentials, use them.
-        //    if (!string.IsNullOrEmpty(_config.Username) && !string.IsNullOrEmpty(_config.Password))
-        //    {
-        //        client.Credentials = new NetworkCredential(_config.Username, _config.Password);
-        //        _logger.LogDebug("Credentials are set in app settings, will leverage for SMTP connection.");
-        //    }
-
-        //    try
-        //    {
-        //        await client.SendMailAsync(message);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        if (ex is SmtpException)
-        //        {
-        //            _logger.LogError("An SMTP exception occurred while attempting to relay mail.");
-        //        }
-        //        else
-        //        {
-        //            _logger.LogError("A general exception occured while attempting to relay mail.");
-        //        }
-
-        //        _logger.LogError(ex.Message);
-        //        return false;
-        //    }
-        //    finally
-        //    {
-        //        message.Dispose();
-        //    }
-
-        //    _logger.LogInformation("Message relay complete.");
-        //    return true;
-        //}
 
 #pragma warning disable 8600
 
@@ -148,6 +43,7 @@
         public async Task<bool> SendEmailSendGrid(MailMessage message, string strCaller = "")
         {
             _logger.LogError($"{strCaller}MailRelayService: SendEmailSendGrid@150 called (caller = {strCaller})");
+            DumpConfigState($"{strCaller}@46: ");
 
             bool bSuccess = false;
             try
@@ -180,6 +76,7 @@
                 _logger.LogError($"{strCaller}MailRelayService: Exception -- {ex.Message}");
             }
 
+            DumpConfigState($"{strCaller}@79: ");
             return bSuccess;
         }
 
@@ -192,6 +89,7 @@
         public async Task<bool> SendEmailSendGrid(MailMessage message, List<EmailAddress> leaTo, string strCaller = "")
         {
             _logger.LogError($"{strCaller}MailRelayService: SendEmailSendGrid@192 called (caller = {strCaller})");
+            DumpConfigState($"{strCaller}@92: ");
 
             bool bSuccess = false;
 
@@ -260,7 +158,188 @@
                 }
             }
 
+            DumpConfigState($"{strCaller}@161: ");
+
             return bSuccess;
+        }
+
+
+        private void DumpConfigState(string strCaller)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (_config == null)
+            {
+                sb.Append("_config is null");
+            }
+            else
+            {
+                // _config.Debug
+                sb.Append($"_config.Debug={_config.Debug.ToString()} $$$ ");
+                //_config.DebugToAddresses
+                string strResult = PutListIntoString(_config.DebugToAddresses);
+                sb.Append($"_config.DebugToAddresses: {strResult} $$$ ");
+
+                //_config.ToAddresses
+                strResult = PutListIntoString(_config.ToAddresses);
+                sb.Append($"_config.ToAddresses: {strResult} $$$ ");
+
+                //_config.MailFromAddress
+                strResult = PutStringIntoString(_config.MailFromAddress);
+                sb.Append($"_config.MailFromAddress: {strResult} $$$ ");
+
+                //_config.MailFromAppName
+                strResult = PutStringIntoString(_config.MailFromAppName);
+                sb.Append($"_config.MailFromAppName: {strResult} $$$ ");
+
+                //_config.ApiKey
+                strResult = PutStringIntoString(_config.ApiKey);
+                sb.Append($"_config.ApiKey: {strResult} $$$ ");
+            }
+
+            string strOutput = sb.ToString();
+            _logger.LogError($"{strCaller}MailRelayService [DumpConfigState]: {strOutput}");
+        }
+
+        private string PutListIntoString(List<string> l)
+        {
+            string strReturn = "empty";
+            if (l != null && l.Count > 0)
+            {
+                strReturn = "[";
+                foreach (string str in l)
+                {
+                    strReturn += $"{str}, ";
+                }
+
+                int i = strReturn.LastIndexOf(",");
+                if (i > 1)
+                {
+                    strReturn = strReturn.Substring(0, i);
+                }
+                strReturn += "]";
+            }
+
+            return strReturn;
+        }
+
+        private string PutStringIntoString(string str)
+        {
+            string strReturn = "empty";
+            if (!string.IsNullOrEmpty(str))
+            {
+                strReturn = $"[{str}]";
+            }
+
+            return strReturn;
         }
     }
 }
+
+
+
+//public async Task<bool> SendEmail(MailMessage message)
+//{
+//    switch (_config.Provider)
+//    {
+//        case "SMTP":
+//            if (!await SendSmtpEmail(message))
+//            {
+//                return false;
+//            }
+
+//            break;
+//        case "SendGrid":
+//            if (!await SendEmailSendGrid(message))
+//            {
+//                return false;
+//            }
+//            break;
+//        default:
+//            _logger.LogError("MailRelayService::SendEmail - Provider key value is invalid");
+//            throw new InvalidOperationException("The configured email provider is invalid.");
+//    }
+//    return true;
+//}
+
+//private async Task<bool> SendSmtpEmail(MailMessage message)
+//{
+//    // Do not proceed further if mail relay is disabled.
+//    if (!_config.Enabled)
+//    {
+//        _logger.LogWarning("Mail Relay is disabled.");
+//        return true;
+//    }
+
+//    // Configure the SMTP client and send the message
+//    var client = new SmtpClient
+//    {
+//        Host = _config.Address,
+//        Port = _config.Port,
+
+//        // Use whatever SSL mode is set.
+//        EnableSsl = _config.EnableSsl,
+//        DeliveryMethod = SmtpDeliveryMethod.Network
+//    };
+
+//    if (_config.EnableSsl)
+//    {
+//        ServicePointManager.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
+//    }
+
+//    _logger.LogDebug($"Email configuration | Server: {_config.Address} Port: {_config.Port} SSL: {_config.EnableSsl}");
+
+//    message.From = new MailAddress(_config.MailFromAddress, "SM Marketplace");
+
+//    // If Mail Relay is in debug mode set all addresses to the configuration file.
+//    if (_config.Debug)
+//    {
+//        _logger.LogDebug($"Mail relay is in debug mode. Redirecting target email to: {string.Join("|", _config.DebugToAddresses)}");
+//        message.To.Clear();
+//        foreach (var address in _config.DebugToAddresses)
+//        {
+//            message.To.Add(address);
+//        }
+//    }
+
+//    else
+//    {
+//        message.To.Clear();
+//        foreach (var address in _config.ToAddresses)
+//        {
+//            message.To.Add(address);
+//        }
+//    }
+
+//    // If the user has setup credentials, use them.
+//    if (!string.IsNullOrEmpty(_config.Username) && !string.IsNullOrEmpty(_config.Password))
+//    {
+//        client.Credentials = new NetworkCredential(_config.Username, _config.Password);
+//        _logger.LogDebug("Credentials are set in app settings, will leverage for SMTP connection.");
+//    }
+
+//    try
+//    {
+//        await client.SendMailAsync(message);
+//    }
+//    catch (Exception ex)
+//    {
+//        if (ex is SmtpException)
+//        {
+//            _logger.LogError("An SMTP exception occurred while attempting to relay mail.");
+//        }
+//        else
+//        {
+//            _logger.LogError("A general exception occured while attempting to relay mail.");
+//        }
+
+//        _logger.LogError(ex.Message);
+//        return false;
+//    }
+//    finally
+//    {
+//        message.Dispose();
+//    }
+
+//    _logger.LogInformation("Message relay complete.");
+//    return true;
+//}
